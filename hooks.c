1df7bf16cafc8bb08a463ecf7cd1e891a28275b6
YOUSSEF-YL <youssef.bachar7@gmail.com>
2022-12-24 20:24:19 +0100

    key_hook

--- hooks.c
+++ hooks.c
@@ -6,7 +6,7 @@
 /*   By: ybachar <ybachar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 12:26:41 by ybachar           #+#    #+#             */
-/*   Updated: 2022/12/24 14:46:41 by ybachar          ###   ########.fr       */
+/*   Updated: 2022/12/24 20:21:31 by ybachar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,22 @@ int	key_hook(char ** map ,int keycode)
     
     if (keycode == 126 || keycode == 13 && map[var.j+1][var.i] != 'E' && map[var.j+1][var.i] != '1') //up
     {
-       map[var.j+1][var.i] =  map[var.j][var.i] ;
+       map[var.j-1][var.i] =  map[var.j][var.i];
        mlx_clear_window(vars.mlx,vars.win);
     }
-    if (keycode == 124 || keycode == 2 && map[var.j][var.i+1] != 'E' && map[var.j][var.i+1] != '1') //r
+    if (keycode == 124 || keycode == 2  && map[var.j][var.i+1] != 'E' && map[var.j][var.i+1] != '1') //r
     {   
         map[var.j][var.i+1] = map[var.j][var.i];
         mlx_clear_window(vars.mlx,vars.win);
     }
-    if (keycode == 125 || keycode == 1) //d
+    if (keycode == 125 || keycode == 1 && map[var.j+1][var.i] != 'E' && map[var.j+1][var.i] != '1') //d
     {
-         mlx_clear_window(vars.mlx,vars.win);
+        map[var.j-1][var.i] = map[var.j][var.i];
+        mlx_clear_window(vars.mlx,vars.win);
     }
-    if (keycode == 123 || keycode == 0) //l
+    if (keycode == 123 || keycode == 0 && map[var.j][var.i- 1] != 'E' && map[var.j][var.i -1] != '1') //l
     {
+        map[var.j][var.i -1] = map[var.j][var.i];
         mlx_clear_window(vars.mlx,vars.win);
     }
     
