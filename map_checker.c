ac29a45f3588414986d1e755649d0281495053d6
YOUSSEF-YL <youssef.bachar7@gmail.com>
2023-01-05 19:23:53 +0100

    norm

--- map_checker.c
+++ map_checker.c
@@ -6,17 +6,15 @@
 /*   By: ybachar <ybachar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:29:16 by yiachar           #+#    #+#             */
-/*   Updated: 2023/01/01 20:28:51 by ybachar          ###   ########.fr       */
+/*   Updated: 2023/01/05 19:22:58 by ybachar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "so_long.h"
-
+#include "so_long.h"
 
 // int c_cherch(char *str ,char c)
 // {
 // 	int i ;
-	
 // 	i = 0;
 // 	while (str[i] != '\0')
 // 	{
@@ -42,13 +40,13 @@
 // 	return (0);
 // }
 
-int somthing(char ** map)
+int	somthing(char **map)
 {
-	t_intvars var;
+	t_intvars	var;
 
 	var.i = 0;
 	var.j = 0 ;
-	while(map[var.j])
+	while (map[var.j])
 	{
 		var.i = 0;
 		while (map[var.j][var.i])
@@ -62,89 +60,86 @@ int somthing(char ** map)
 	return (1);
 }
 
-int check_liens_l(char **map)
+int	check_liens_l(char **map)
 {
-	t_intvars intvar;
+	t_intvars	intvar;
+	int			i;
 
-    int i;
 	intvar.i = 1;
-	intvar.j = map_lines("map.ber")-1;
-	
-    while (intvar.i < intvar.j)
+	intvar.j = map_lines("map.ber") - 1;
+	while (intvar.i < intvar.j)
 	{
-		if (ft_strlen(map[0])-1 != ft_strlen(map[intvar.i])-1)
-			return (0);	
+		if (ft_strlen(map[0]) - 1 != ft_strlen(map[intvar.i]) - 1)
+			return (0);
 		intvar.i++;
 	}
- 	return(1);
+	return (1);
 }
 
-int check_walls(char **map)
+int	check_walls(char **map)
 {
-	t_intvars intvar;
-	
-    intvar.j = 0;
+	t_intvars	intvar;
+
+	intvar.j = 0;
 	intvar.i = 0;
-		while (intvar.i < ft_strlen(map[intvar.j])-1) //-1
-		{
-			if (map[0][intvar.i] != '1') //top_l
-				return (0);
-			if (map[map_lines("map.ber")-1][intvar.i] != '1') // last_l
-				return (0);
-			 intvar.i++;
-		}
-	intvar.e = ft_strlen(map[intvar.j])-2;
+	while (intvar.i < ft_strlen(map[intvar.j]) - 1)
+	{
+		if (map[0][intvar.i] != '1')
+			return (0);
+		if (map[map_lines("map.ber") - 1][intvar.i] != '1')
+			return (0);
+			intvar.i++;
+	}
+	intvar.e = ft_strlen(map[intvar.j]) - 2;
 	while (map[intvar.j])
 	{
-			if (map[intvar.j][intvar.e] != '1') //R_l
-				return (0);	
-			if (map[intvar.j][0] != '1') //l_l
-				return (0);
+		if (map[intvar.j][intvar.e] != '1')
+			return (0);
+		if (map[intvar.j][0] != '1')
+			return (0);
 		intvar.j++;
 	}
- return(1);
+	return (1);
 }
 
-
-char ** check_path(char ** map ,int j , int i)
+char	**check_path(char	**map, int j, int i)
 {
 	if (map[j][i] == 'P')
+	{
+		if (map[j +1][i] == '0' || map[j +1][i] == 'C' || map[j +1][i] == 'E')
 		{
-			if (map[j+1][i] == '0' || map[j+1][i] == 'C' || map[j+1][i] == 'E')//up
-			{
-			 	map[j+1][i] = 'P';
-			  check_path(map,j+1,i );
-			}
-			if (map[j-1][i] == '0' || map[j-1][i] == 'C' || map[j-1][i] == 'E')//down
-			{
-				map[j-1][i] = 'P';
-				check_path(map,j-1 , i);
-			}
-			if (map[j][i+1] == '0' || map[j][i+1] == 'C' || map[j][i+1] == 'E')//r
-			{
-				map[j][i+1] = 'P';
-				check_path(map,j , i+1);
-			}
-			if (map[j][i-1] == '0' || map[j][i-1] == 'C' || map[j][i-1] == 'E')//l
-			{
-				map[j][i-1] = 'P';
-				check_path(map, j, i-1);
-			}
+			map[j + 1][i] = 'P';
+			check_path(map, j +1, i);
 		}
+		if (map[j -1][i] == '0' || map[j -1][i] == 'C' || map[j -1][i] == 'E')
+		{
+			map[j -1][i] = 'P';
+			check_path(map, j -1, i);
+		}
+		if (map[j][i +1] == '0' || map[j][i +1] == 'C' || map[j][i +1] == 'E')
+		{
+			map[j][i +1] = 'P';
+			check_path(map, j, i +1);
+		}
+		if (map[j][i -1] == '0' || map[j][i -1] == 'C' || map[j][i -1] == 'E' )
+		{
+			map[j][i -1] = 'P';
+			check_path(map, j, i -1);
+		}
+	}
 	j++;
-	//printf("%c\n\n",map[13][1]);
 	return (map);
 }
 
-int map_req(char ** map)
+int	map_req(char **map)
 {
-	struct vars var;
+	struct vars	var;
 
 	var.p = 0;
 	var.e = 0;
 	var.c = 0;
 	var.j = 0;
-	while(map[var.j])
+	while (map[var.j])
 	{
 		var.i = 0;
 		while (map[var.j][var.i])
@@ -155,13 +150,13 @@ int map_req(char ** map)
 				var.e = var.e +1;
 			if (map[var.j][var.i] == 'C')
 				var.c = var.c +1;
-		 var.i++;
+			var.i++;
 		}
 		var.j++;
 	}
-	if(var.p < 1 || var.c < 1 || var.e < 1 || var.p > 1 || var.e > 1)
+	if (var.p < 1 || var.c < 1 || var.e < 1 || var.p > 1 || var.e > 1)
 		return (0);
-	return(1);
+	return (1);
 }
 
 int  is_map_valid(char ** map)
